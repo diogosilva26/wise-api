d362073d55b712183e3e7c008acd68e1501ef0a8
caio-couto <cavalcantecaio.couto@gmail.com>
2023-12-08 01:16:34 -0300

    feat: implement middleware errors handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
+import AppError from "@shared/erros/AppError";
 
 const app = express();
 
@@ -9,6 +10,24 @@ const PORT: number = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) =>
+{
+    console.log(error.stack);
+    if (error instanceof AppError)
+    {
+        return res.status(error.statusCode).json(
+        {
+            status: "error",
+            message: error.message,
+        });
+    }
+
+    return res.status(500).json(
+    {
+        status: "error",
+        message: "Internal server error."
+    });
+});
 
 app.listen(PORT, () => 
 {
