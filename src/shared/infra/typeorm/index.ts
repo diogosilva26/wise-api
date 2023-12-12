4aa3b1d37097defa5d3a172b08be7ad92f19e7e9
caio-couto <cavalcantecaio.couto@gmail.com>
2023-12-12 02:21:43 -0300

    feat: create UserTokens migration

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,9 +2,11 @@ import { DataSource } from "typeorm";
 
 //Entidades
 import User from "@modules/users/infra/typeorm/entities/User";
+import UserToken from "@modules/users/infra/typeorm/entities/UserToken";
 
 //Migrações
 import { CreateUsers1702013688239 } from "./migrations/1702013688239-CreateUsers";
+import { CreateUserTokens1697822854225 } from "./migrations/1702339903467-CreateUserTokens";
 
 export const dataSource = new DataSource(
 {
@@ -14,6 +16,6 @@ export const dataSource = new DataSource(
     username: "postgres",
     password: "123456",
     database: "wiseapi",
-    entities: [User],
-    migrations: [CreateUsers1702013688239],
+    entities: [User, UserToken],
+    migrations: [CreateUsers1702013688239, CreateUserTokens1697822854225],
 });
